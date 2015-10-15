3101945896626432cbf9eebadced5bf4cb4f0eb0
Xu Fasheng <fasheng.xu@gmail.com>
2015-10-15 10:51:09 +0800

    refactor granite code tree
    Change-Id: I4bd4c50046a8bebba0ca73c5bcb558c68f631095

--- src/Granite/update_granite.sh
+++ src/Granite/update_granite.sh
@@ -3,9 +3,11 @@
 granite_bzr_dir="${1}"
 
 if [ ! -d "${granite_bzr_dir}" ]; then
-    echo "==> need granite.bzr source directory"
+    echo "==> need granite.bzr source directory, just clone the code through 'bzr branch lp:granite'"
     exit 1
 fi
 
 cp -rvf "${granite_bzr_dir}"/lib/{style-classes.vala,Drawing,Services} .
+
+mkdir -p Widgets
 cp -vf "${granite_bzr_dir}"/lib/Widgets/Utils.vala ./Widgets/
